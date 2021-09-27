e84944f5e4e154f6c61d25351e2af3bae0bfe148
Zack Pollard <zackpollard@ymail.com>
2021-09-28 00:53:22 +0100

    fix: lowercase availableTimes command to availabletimes

--- src/commands/availableTimes.ts
+++ src/commands/availableTimes.ts
@@ -8,7 +8,7 @@ import { getLogin } from '../storage/logins';
 
 export function availableTimesCommand(telegramBot: TelegramBot) {
   telegramBot.onText(
-    /\/availableTimes (Manor|Castle) .*/,
+    /\/availabletimes (Manor|Castle) .*/,
     async (msg: Message, match) => {
       const chatId = msg.chat.id;
 
