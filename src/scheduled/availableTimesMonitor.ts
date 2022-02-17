2cbf1948a6eefeb1dcd38e8491c8de4c09e45368
Zack Pollard <zack.pollard@moonpig.com>
2022-02-17 13:10:21 +0000

    feat: allow monitoring multiple courses and time ranges

--- src/scheduled/availableTimesMonitor.ts
+++ src/scheduled/availableTimesMonitor.ts
@@ -3,85 +3,97 @@ import { AsyncTask, SimpleIntervalJob, ToadScheduler } from 'toad-scheduler';
 import TelegramBot from 'node-telegram-bot-api';
 import rp from 'request-promise';
 
-import { getMonitors } from '../storage/monitors';
+import { getAllMonitors } from '../storage/monitors';
 import { getCourseAvailability, login } from '../requests/golfBooking';
 import { getLogin } from '../storage/logins';
 
-const cache: { [key: number]: string[] } = {};
+const cache: { [key: string]: string[] } = {};
 
 export function scheduledAvailableTimesMonitor(bot: TelegramBot) {
   const scheduler = new ToadScheduler();
   const task = new AsyncTask('availableTimesMonitor', async () => {
-    const monitors = await getMonitors();
-    for (const key in monitors) {
-      if (Object.hasOwnProperty.call(monitors, key)) {
-        const userId = Number.parseInt(key, 10);
-        const monitor = monitors[key];
-        const { course, startDate, endDate } = monitor;
-        const request = rp.defaults({ jar: true, followAllRedirects: true });
+    const monitors = await getAllMonitors();
+    for (const userKey in monitors) {
+      if (Object.hasOwnProperty.call(monitors, userKey)) {
+        const userId = Number.parseInt(userKey, 10);
+        const userMonitors = monitors[userKey];
+        const request = rp.defaults({
+          jar: true,
+          followAllRedirects: true
+        });
         const credentials = await getLogin(userId);
         await login(request, {
           username: credentials.username,
           password: credentials.password
         });
-        let availability = await getCourseAvailability(request, {
-          course,
-          date: startDate
-        });
-        const startTime = `${startDate
-          .getUTCHours()
-          .toString()
-          .padStart(2, '0')}:${startDate
-          .getUTCMinutes()
-          .toString()
-          .padStart(2, '0')}`;
-        const endTime = `${endDate
-          .getUTCHours()
-          .toString()
-          .padStart(2, '0')}:${endDate
-          .getUTCMinutes()
-          .toString()
-          .padStart(2, '0')}`;
-        availability = availability.filter((el) => {
-          return el > startTime && el < endTime;
-        });
+        for (const monitorKey in userMonitors) {
+          if (Object.hasOwnProperty.call(monitors, userKey)) {
+            const { course, startDate, endDate } = userMonitors[monitorKey];
+            const cacheKey = `${course}${startDate.toISOString()}${endDate.toISOString()}`;
 
-        if (!cache[userId]) {
-          cache[userId] = availability;
-          return;
-        }
-
-        const cacheAvailability = cache[userId];
-
-        for (const timeKey in availability) {
-          if (Object.hasOwnProperty.call(availability, timeKey)) {
-            const time = availability[timeKey];
-            if (cacheAvailability.includes(time)) continue;
-            let message = '<b>New Available Times</b>\n';
-            message += `<b>Course:</b> ${course}\n`;
-            message += `<b>Search Date:</b> ${startDate.getDate()}/${
-              startDate.getMonth() + 1
-            }/${startDate.getFullYear()}\n`;
-            message += `<b>Search Start Time</b> ${startTime}\n`;
-            message += `<b>Search End Time</b> ${endTime}\n`;
-
-            availability.forEach((availabilityTime) => {
-              message += `\n<b>Time:</b> ${availabilityTime}`;
+            let availability = await getCourseAvailability(request, {
+              course,
+              date: startDate
+            });
+            const startTime = `${startDate
+              .getUTCHours()
+              .toString()
+              .padStart(2, '0')}:${startDate
+              .getUTCMinutes()
+              .toString()
+              .padStart(2, '0')}`;
+            const endTime = `${endDate
+              .getUTCHours()
+              .toString()
+              .padStart(2, '0')}:${endDate
+              .getUTCMinutes()
+              .toString()
+              .padStart(2, '0')}`;
+            availability = availability.filter((el) => {
+              return el > startTime && el < endTime;
             });
 
-            await bot.sendMessage(userId, message, { parse_mode: 'HTML' });
-            break;
+            if (!cache[cacheKey]) {
+              cache[cacheKey] = availability;
+              continue;
+            }
+
+            const cacheAvailability = cache[cacheKey];
+
+            const newTimes: string[] = [];
+
+            for (const timeKey in availability) {
+              if (Object.hasOwnProperty.call(availability, timeKey)) {
+                const time = availability[timeKey];
+                if (cacheAvailability.includes(time)) continue;
+                newTimes.push(time);
+              }
+            }
+
+            if (newTimes.length > 0) {
+              let message = '<b>New Available Times</b>\n';
+              message += `<b>Course:</b> ${course}\n`;
+              message += `<b>Search Date:</b> ${startDate.getDate()}/${
+                startDate.getMonth() + 1
+              }/${startDate.getFullYear()}\n`;
+              message += `<b>Search Start Time</b> ${startTime}\n`;
+              message += `<b>Search End Time</b> ${endTime}\n`;
+
+              newTimes.forEach((availabilityTime) => {
+                message += `\n<b>New Available Time:</b> ${availabilityTime}`;
+              });
+
+              await bot.sendMessage(userId, message, { parse_mode: 'HTML' });
+            }
+
+            // eslint-disable-next-line require-atomic-updates
+            cache[cacheKey] = availability;
           }
         }
-
-        cache[userId] = availability;
       }
     }
   });
 
-  const job = new SimpleIntervalJob(
-    { seconds: 15, runImmediately: true },
-    task
-  );
+  const job = new SimpleIntervalJob({ minutes: 1, runImmediately: true }, task);
   scheduler.addSimpleIntervalJob(job);
 }
