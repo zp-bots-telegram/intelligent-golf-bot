88a345dd15b661761d55819f874a46a1b4407126
Zack Pollard <zack.pollard@moonpig.com>
2023-07-28 18:46:33 +0100

    ci: downgrade semantic release to old versions due to npm issues

--- src/scheduled/autoBookingsMonitor.ts
+++ src/scheduled/autoBookingsMonitor.ts
@@ -135,7 +135,7 @@ export function scheduledAutoBookingsMonitor(bot: Bot): void {
 
   const autoBookingNightly = new CronJob(
     {
-      cronExpression: '* 0,1,2,3,4,5,6,7,8,9,10 0 * * *'
+      cronExpression: '* 0,1,2,3,4,5,6,7,8,9,10 23,0 * * *'
     },
     autoBookingJob,
     {
